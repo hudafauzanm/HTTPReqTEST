fc3a6fe9daffecb56a1de954eb8b56613da402d1
Huda Fauzan Murtadho <hudafauzanm@gmail.com>
2020-01-23 12:09:18 +0700

    Soal 4 done

---
+++ soal4/Result.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+
+namespace Results
+{
+    public class Result
+    {
+        public double popularity { get; set; }
+        public int id { get; set; }
+        public bool video { get; set; }
+        public int vote_count { get; set; }
+        public double vote_average { get; set; }
+        public string title { get; set; }
+        public string release_date { get; set; }
+        public string original_language { get; set; }
+        public string original_title { get; set; }
+        public string[] genre_ids { get; set; }
+        public string backdrop_path { get; set; }
+        public bool adult { get; set; }
+        public string overview { get; set; }
+        public string poster_path { get; set; }
+     }
+
+public class Data
+ {
+    public int page { get; set; }
+    public int total_results { get; set; }
+    public int total_pages { get; set; }
+    public List<Result> results { get; set; }
+ }
+}
