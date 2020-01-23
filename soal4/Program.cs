48a8630f408e0e71b8f8f03914789b3a9ea0b207
Huda Fauzan Murtadho <hudafauzanm@gmail.com>
2020-01-23 12:08:55 +0700

    Soal 4 done bro

--- soal4/Program.cs
+++ soal4/Program.cs
@@ -3,54 +3,85 @@ using System.Net.Http;
 using Newtonsoft.Json;
 using System.Threading;
 using System.Threading.Tasks;
+using Results;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace soal4
 {
     class Program
     {
-        public static async Task<string> Movies()
+        public static async Task Movies()
         {
             HttpClient client = new HttpClient();
             HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get,"https://api.themoviedb.org/3/search/movie?api_key=8ade8bea9d0c658c52b0c7e7e2991de9&language=id&query=Indonesia&page=1&include_adult=true&region=id-ID");
             HttpResponseMessage response = await client.SendAsync(request);
             var json = await response.Content.ReadAsStringAsync();
-            return json;
+            var list = JsonConvert.DeserializeObject<Data>(json);
+
+            var movies = from l in list.results select l.original_title;
+
+            foreach(var m in movies)
+            {
+                Console.WriteLine("Judul : "+m);
+            }
         }
 
-        public static async Task<string> Keanu()
+        public static async Task Keanu()
         {
             HttpClient client = new HttpClient();
-            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get,"https://api.themoviedb.org/3/search/person?api_key=8ade8bea9d0c658c52b0c7e7e2991de9&language=en-US&query=Keanu&page=1&include_adult=false");
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get,"https://api.themoviedb.org/3/discover/movie?api_key=8ade8bea9d0c658c52b0c7e7e2991de9&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_cast=6384");
             HttpResponseMessage response = await client.SendAsync(request);
             var json = await response.Content.ReadAsStringAsync();
-            return json;   
+            var list = JsonConvert.DeserializeObject<Data>(json);
+
+            var movies = from l in list.results select l.original_title;
+
+            foreach(var m in movies)
+            {
+                Console.WriteLine("Judul : "+m);
+            }   
         }
-        public static async Task<string> Robert()
+        public static async Task Robert()
         {
             HttpClient client = new HttpClient();
             HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get,"https://api.themoviedb.org/3/discover/movie?api_key=8ade8bea9d0c658c52b0c7e7e2991de9&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_cast=1136406%2C3223");
             HttpResponseMessage response = await client.SendAsync(request);
             var json = await response.Content.ReadAsStringAsync();
-            return json;   
+            var list = JsonConvert.DeserializeObject<Data>(json);
+
+            var movies = from l in list.results select l.original_title;
+
+            foreach(var m in movies)
+            {
+                Console.WriteLine("Judul : "+m);
+            }      
         }
-        public static async Task<string> List()
+        public static async Task List()
         {
             HttpClient client = new HttpClient();
-            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get,"https://api.themoviedb.org/3/discover/movie?api_key=8ade8bea9d0c658c52b0c7e7e2991de9&language=en-US&sort_by=vote_count.desc&include_adult=false&include_video=false&page=1&primary_release_year=2016&vote_count.lte=7.5");
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get,"https://api.themoviedb.org/3/discover/movie?api_key=8ade8bea9d0c658c52b0c7e7e2991de9&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=2016&vote_average.gte=7.5");
             HttpResponseMessage response = await client.SendAsync(request);
             var json = await response.Content.ReadAsStringAsync();
-            return json;   
+            var list = JsonConvert.DeserializeObject<Data>(json);
+
+            var movies = from l in list.results select l.title;
+
+            foreach(var m in movies)
+            {
+                Console.WriteLine("Judul : "+m);
+            }   
         }
         public static async Task Main(string[] args)
         {
            Console.WriteLine("==============Get 10+ titles of Indonesian movies================");
-           Console.WriteLine(await Movies()); 
+           await Movies(); 
            Console.WriteLine("===============Get movie list played by Keanu Reeves=============");
-           Console.WriteLine(await Keanu());
+           await Keanu();
            Console.WriteLine("===============Get movie list played by Robert Downey Jr. and Tom Holland=============");
-           Console.WriteLine(await Robert());
+           await Robert();
            Console.WriteLine("===============Get popular movie list that released on 2016 and the votes above 7.5=============");
-           Console.WriteLine(await List());
+           await List();
             
         }
     }
